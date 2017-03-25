9d44b568c2338b3986a81da4c261e078c63cba93
Marcel Oomens <mail@marceloomens.com>
2017-03-25 21:20:47 +0100

    Prepare for Heroku deployment
    Signed-off-by: Marcel Oomens <mail@marceloomens.com>

--- lib/lube.ex
+++ lib/lube.ex
@@ -8,11 +8,26 @@ defmodule Lube do
 
   def start(_type, _args) do
     children = [
-      Plug.Adapters.Cowboy.child_spec(:http, Lube.Router, [], port: 8080)
+      Plug.Adapters.Cowboy.child_spec(:http, Lube.Router, [], port: port())
     ]
 
     Logger.info "Started application"
 
     Supervisor.start_link(children, strategy: :one_for_one)
   end
+
+  defp port do
+    case System.get_env("PORT") do
+      nil ->
+        8080
+      str ->
+        try do
+          String.to_integer(str)
+        rescue
+          ArgumentError ->
+            8080
+        end
+    end
+  end
+
 end
